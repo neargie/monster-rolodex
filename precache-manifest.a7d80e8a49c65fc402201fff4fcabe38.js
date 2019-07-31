abed6e4dd23f7c89c6f6249d5816ec99795fd386
Egi Bhakti Bhuana <neargie@gmail.com>
2019-07-31 22:00:43 +0700

    Updates

---
+++ precache-manifest.a7d80e8a49c65fc402201fff4fcabe38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0618eaacd1c77080525673176ae6fbba",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b733380d810c4aaa95a5",
+    "url": "/monster-rolodex/static/css/main.2c0871c0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b733380d810c4aaa95a5",
+    "url": "/monster-rolodex/static/js/main.8adb4d84.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
